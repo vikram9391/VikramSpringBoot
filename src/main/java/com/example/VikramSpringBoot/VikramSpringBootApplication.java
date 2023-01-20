d829147380869a42eb34b72e767045bb937a0548
vikram9391 <40818671+vikram9391@users.noreply.github.com>
2023-01-20 18:01:20 +0530

    Add files via upload

---
+++ src/main/java/com/example/VikramSpringBoot/VikramSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.VikramSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikramSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikramSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VikramSpringBoot/VikramSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VikramSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikramSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
